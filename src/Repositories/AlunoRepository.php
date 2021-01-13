244e7d8fb5b083e8cf732e2795803d4ac8f4b004
Ismael Barboza <ismael@inovadora.com.br>
2021-01-13 13:22:13 -0300

    Change dql to queryBuider on AlunoRepository

--- src/Repositories/AlunoRepository.php
+++ src/Repositories/AlunoRepository.php
@@ -7,14 +7,12 @@ use Doctrine\ORM\EntityRepository;
 class AlunoRepository extends EntityRepository {
 
   public function buscarCursosPorAluno () {
-    $className = Aluno::class;
-    $dql = "SELECT aluno, telefones, cursos FROM $className as aluno 
-                                            JOIN aluno.telefones telefones 
-                                            JOIN aluno.cursos cursos";
-    /** @var Aluno[] $alunos */
-    $alunos= $this->getEntityManager()
-                  ->createQuery($dql)
-                  ->getResult();
-    return $alunos;
+    $query = $this->createQueryBuilder('aluno')
+         ->join('aluno.telefones','t')
+         ->join('aluno.cursos', 'c')
+         ->addSelect('t')
+         ->addSelect('c')
+         ->getQuery();
+    return $query->getResult();
   }
 }
